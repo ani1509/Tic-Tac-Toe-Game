3b443b3f309f762285ae6266283ff9ac0ed2acc1
ani1509 <aniruddh01gupta@gmail.com>
2024-11-22 00:21:15 +0530

    again add base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/xTic-Tac-Toe-Game",
+  base: "/Tic-Tac-Toe-Game",
 
   plugins: [react()],
 });
